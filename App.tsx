d74afb458c6073e11896c2962151342de424e7c5
user <babacardiouf29@gmail.com>
2026-03-01 15:29:19 +0000

    replace HashRouter with  BrowseRouter

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route, useLocation, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Services from './components/Services';
